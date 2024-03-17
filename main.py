b245067b76ec34864422a155de894f9648a27255
jamesfimmer <jamesfimmer@protonmail.com>
2024-03-17 18:43:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import minecraft_launcher_lib
 import subprocess
-
+import requests
+import os
+from zipfile import ZipFile
+import shutil
 
 def main():
     minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory().replace('.minecraft', '.jfcraft')
     callback = {
         "setStatus": lambda text: print(text, end='\n'),
     }
-    minecraft_version = input()
-    minecraft_launcher_lib.install.install_minecraft_version(minecraft_version, minecraft_directory, callback)
+    print("Установка сборки ...")
+    #minecraft_launcher_lib.forge.install_forge_version('1.19.2-43.3.5', minecraft_directory, callback)
     options = minecraft_launcher_lib.utils.generate_test_options()
-    print("Идёт крутой запуск майнкрафта")
-    subprocess.run(minecraft_launcher_lib.command.get_minecraft_command(minecraft_version, minecraft_directory, options))
+    mods_url = 'https://github.com/jamesfimmer/JFCRAFT-MODS/raw/main/mods.zip'
+    mods_folder = minecraft_directory
+    print(mods_folder)
+    response = requests.get(mods_url)
+    with open('mods.zip', 'wb') as f:
+        f.write(response.content)
+        # Распаковка архива
+    shutil.unpack_archive('mods.zip', mods_folder, 'zip')
+
+    os.remove('mods.zip')
+
+    print("Запуск")
+    subprocess.run(minecraft_launcher_lib.command.get_minecraft_command('1.19.2-forge-43.3.5', minecraft_directory, options))
     return
 
 
