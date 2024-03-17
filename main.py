2eb9c6340cf5884835aff47ac4a5ebe97c916914
jamesfimmer <jamesfimmer@protonmail.com>
2024-03-17 16:34:57 +0400

    Just start

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import minecraft_launcher_lib
+import subprocess
 
 
 def main():
+    minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory().replace('.minecraft', '.jfcraft')
+    callback = {
+        "setStatus": lambda text: print(text, end='\n'),
+    }
+    minecraft_version = input()
+    minecraft_launcher_lib.install.install_minecraft_version(minecraft_version, minecraft_directory, callback)
+    options = minecraft_launcher_lib.utils.generate_test_options()
+    print("Идёт крутой запуск майнкрафта")
+    subprocess.run(minecraft_launcher_lib.command.get_minecraft_command(minecraft_version, minecraft_directory, options))
     return
 
 
