9d42bf05179b3a74e4f942ff09b099559c71ddc4
jamesfimmer <jamesfimmer@protonmail.com>
2024-03-19 16:33:39 +0400

    upd

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from zipfile import ZipFile
 import shutil
 
+
 def main():
     minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory().replace('.minecraft', '.jfcraft')
     callback = {
@@ -14,19 +15,19 @@ def main():
     #minecraft_launcher_lib.forge.install_forge_version('1.19.2-43.3.5', minecraft_directory, callback)
     options = minecraft_launcher_lib.utils.generate_test_options()
     mods_url = 'https://github.com/jamesfimmer/JFCRAFT-MODS/raw/main/mods.zip'
-    mods_folder = minecraft_directory
+    mods_folder = minecraft_directory + '\\mods'
     print(mods_folder)
-    response = requests.get(mods_url)
-    with open('mods.zip', 'wb') as f:
-        f.write(response.content)
-        # Распаковка архива
+    # response = requests.get(mods_url)
+    # with open('mods.zip', 'wb') as f:
+    #    f.write(response.content)
+    # Распаковка архива
     shutil.unpack_archive('mods.zip', mods_folder, 'zip')
 
-
-    os.remove('mods.zip')
+    # os.remove('mods.zip')
 
     print("Запуск")
-    subprocess.run(minecraft_launcher_lib.command.get_minecraft_command('1.19.2-forge-43.3.5', minecraft_directory, options))
+    subprocess.run(
+        minecraft_launcher_lib.command.get_minecraft_command('1.19.2-forge-43.3.5', minecraft_directory, options))
     return
 
 
